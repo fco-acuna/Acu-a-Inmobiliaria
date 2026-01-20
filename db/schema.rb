00b33caaf6789503ba3a04715ecf228f0d0d2fa0
Francisco <facuna490@gmail.com>
2026-01-20 12:41:57 -0600

    ajustes para desarrollo

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_22_143049) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
+ActiveRecord::Schema[7.1].define(version: 2026_01_06_212318) do
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
     t.string "email", default: "", null: false
@@ -28,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_22_143049) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
@@ -63,14 +61,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_22_143049) do
   create_table "properties", force: :cascade do |t|
     t.integer "account_id"
     t.string "direccion"
+    t.string "colonia"
     t.integer "cuartos"
-    t.float "banos"
+    t.integer "banos"
     t.string "extras"
-    t.float "precio"
+    t.integer "precio"
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "colonia"
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
